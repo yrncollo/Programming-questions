3c17125556d299f57f5d93439b00e01d40d5da3f
YrnCollo <ngenocollins599@gmail.com>
2022-05-22 10:34:01 +0300

    Finding the area of a right angle triangle

---
+++ Geometric_Measurements/Area_of_a_right_angle_triangle.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+	int base,hieght;
+	float answer;
+	printf("Enter the base of the triangle: ");
+	scanf("%d",&base);
+	printf("Enter the hieght of the triangle: ");
+	scanf("%d",&hieght);
+
+	answer=(0.5*base*hieght);
+	printf("The area of the right angle triangle is %.2f ",answer);
+	return 0;
+}
+
