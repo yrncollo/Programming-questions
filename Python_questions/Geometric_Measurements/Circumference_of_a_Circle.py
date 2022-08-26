5d8aa19e70059ff19c05dfe3f65234a6a8179779
YrnCollo <kngenocollins@gmail.com>
2022-08-26 17:16:53 +0300

    circumference of a circle

---
+++ Python_questions/Geometric_Measurements/Circumference_of_a_Circle.py
@@ -0,0 +1,6 @@
+import math
+# finding the circumference of a circle
+radius = input("Enter the radius of the circle: ")
+radius = int(radius)
+circum = 2 * math.pi * radius
+print(f"The circumference of the circle is: {circum}")
