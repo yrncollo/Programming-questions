9fc41b2d793aac920716019b8233ef8027bf0f15
YrnCollo <kngenocollins@gmail.com>
2022-08-26 11:21:46 +0300

    made corrections

--- Python_questions/Geometric_Measurements/Area_right_ange_triangle.py
+++ Python_questions/Geometric_Measurements/Area_right_ange_triangle.py
@@ -1,5 +1,5 @@
-#finding the area of a right angle triangle
-#formula : 0.5 * base * hieght
-base , hieght = input("Enter the base and hieght respectively: ").split()
-area = 0.5 * int(base) * int(hieght)
+# finding the area of a right angle triangle
+# formula : 0.5 * base * height
+base, height = input("Enter the base and height respectively: ").split()
+area = 0.5 * int(base) * int(height)
 print(f"The area of the triangle is: {area}")
