35ea0d809e0f676218d6cef06b331fc9fdf08020
YrnCollo <kngenocollins@gmail.com>
2022-08-26 18:14:20 +0300

    Area of a Rectangle

---
+++ Python_questions/Geometric_Measurements/Area_of_a_Rectangle.py
@@ -0,0 +1,4 @@
+# finding the Area of a Rectangle
+length, width = input("Enter the length and width of the rectangle: ").split()
+area = int(length) * int(width)
+print(f"The Area of the Rectangle is: {area}")
