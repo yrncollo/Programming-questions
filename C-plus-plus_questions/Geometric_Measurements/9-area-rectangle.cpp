88c752ebdf2c27210d02d1d948e10796ee356570
yrncollo <ngenocollins599@gmail.com>
2023-02-10 14:48:57 +0300

    area of a triangle

---
+++ C-plus-plus_questions/Geometric_Measurements/9-area-rectangle.cpp
@@ -0,0 +1,12 @@
+// formular of finding area of rectangle
+// lenght * width
+#include<iostream>
+int main(){
+	int len, width, area;
+	std::cout << "Enter the lenght and width of the triangle: ";
+	std::cin >> len >> width;
+	area = len * width;
+	std::cout << "The area of the rectangle is: " << area;
+
+	return 0;
+}
