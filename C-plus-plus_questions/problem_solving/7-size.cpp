0010f14355c50bbc116998061e9b522460867f2a
yrncollo <ngenocollins599@gmail.com>
2023-02-23 12:42:58 +0300

    Find the Size of int, float, double and char

---
+++ C-plus-plus_questions/problem_solving/7-size.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	double a;
+	int b;
+	float c;
+	char d;
+
+	std::cout << "The size of double is: " << sizeof(a) << "\n";
+	std::cout << "The size of int is: " << sizeof(b)<< "\n";
+	std::cout << "The size of float is: " << sizeof(c)<< "\n";
+	std::cout << "The size of char is: " << sizeof(d)<< "\n";
+}
