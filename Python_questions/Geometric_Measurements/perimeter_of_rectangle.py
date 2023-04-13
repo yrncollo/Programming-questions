c1ef2de5c5b52807edefaa0bbacdf106b9405205
yrncollo <ngenocollins599@gmail.com>
2023-04-13 18:59:15 +0300

    finding remainder and quotient

--- Python_questions/Geometric_Measurements/perimeter_of_rectangle.py
+++ Python_questions/Geometric_Measurements/perimeter_of_rectangle.py
@@ -1,4 +1,5 @@
 # perimeter of a rectangle
-num1 , num2 = input("Enter the width and Length: ").split()
+num1, num2 = input("Enter the width and Length: ").split()
+"""takes the width and length of a rectangle and calculates the perimeter"""
 perimeter = (int(num1) + int(num2)) * 2 
 print(f"the perimeter is: {perimeter}")
---
+++ Python_questions/problem_solving/6-quotient-rem.py
@@ -0,0 +1,8 @@
+Num1 = int(input("Enter first no: "))
+Num2 = int(input("Enter second no: "))
+
+quotient = Num1 / Num2
+remainder = Num1 % Num2
+
+print(f"Quotient: {quotient}")
+print(f"Remainder: {remainder}")
